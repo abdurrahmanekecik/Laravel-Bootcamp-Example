a8dca39ec53d859a80cbaa30cebaf3c0f6431c30
Abdurrahman Ekecik <abdurrahmanekecik@gmail.com>
2023-02-23 18:54:28 +0300

    Policy ile genel güncellemeler yapılabilir

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -56,6 +56,11 @@ class ChirpController extends Controller
     public function edit(Chirp $chirp)
     {
         //
+        $this->authorize('update', $chirp);// authorize ile basit spatie yapılabilir
+
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
     }
 
     /**
@@ -64,8 +69,16 @@ class ChirpController extends Controller
     public function update(Request $request, Chirp $chirp)
     {
         //
-    }
+        $this->authorize('update', $chirp);
 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
     /**
      * Remove the specified resource from storage.
      */
